3219d58216b44e3ec300d8b3286174e160f30afb
XtremeBlaze777 <ratikmathur@gmail.com>
2023-01-24 12:08:11 -0500

    Sockets send cl inputs now

--- receiver.py
+++ receiver.py
@@ -26,7 +26,14 @@ with socket.socket(family=INTERNET_SOCKET, type=TCP) as host:
 
         # receive data from the client
         while True:
-            data = client.recv(max_bytes)
-            if not data:
+            data = client.recv(max_bytes).decode()
+            if data == '-1':
+                print("Did not receive an integer between 1 and 100; closing server")
                 break
-            client.sendall(data)
+            else:
+                num2 = int(data)
+            if not (num2 >= 1 and num2 <= 100):
+                print("You must provide an integer between 1 and 100\nPlease rerun both the server and client")
+                break
+            client.sendall(b"7") # random integer between 1-100 sent back to client; b refers to "bytes-like object"
+            break
--- sender.py
+++ sender.py
@@ -1,6 +1,7 @@
 # Tutorials used: https://realpython.com/python-sockets/ , https://docs.python.org/3/library/socket.html
 
 import socket
+import sys
 
 # constants
 TCP = socket.SOCK_STREAM
@@ -13,9 +14,21 @@ host_ip = "127.0.0.1" # localhost
 host_port = 65432 # random port > 5000
 ADDRESS = (host_ip, host_port)
 
+exit_flag = False
+
 with socket.socket(family=INTERNET_SOCKET, type=TCP) as client:
     client.connect(ADDRESS)
-    client.sendall(b"Hello server!") # b refers to "bytes-like object," not string
-    data = client.recv(max_bytes)
 
-print(f"Received {data!r}")
+    try:
+        num2 = int(sys.argv[1])
+        client.sendall(str(num2).encode("ascii")) # encode turns it into "bytes-like object"
+        num1 = int(client.recv(max_bytes).decode())
+    except (IndexError, ValueError):
+        print("You must provide an integer between 1 and 100\nPlease rerun both the server and client")
+        client.sendall(b'-1')
+        exit_flag = True
+        
+if exit_flag:
+    exit(1)
+else:
+    print(f"Sum of Values: {num1+num2}")
