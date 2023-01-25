280053c972a1c20f1f322653a8ff362f23961a02
XtremeBlaze777 <ratikmathur@gmail.com>
2023-01-24 22:03:06 -0500

    Server closes after first connection

--- server.py
+++ server.py
@@ -25,21 +25,18 @@ with socket.socket(family=INTERNET_SOCKET, type=TCP) as host:
     client, client_ip = host.accept()
     
     with client:
-
         # receive data from the client
-        while True:
-            client_packet = json.loads(client.recv(max_bytes).decode())
-            if not client_packet:
-                print("Did not receive an integer between 1 and 100; closing server")
-                break
+        client_packet = json.loads(client.recv(max_bytes).decode())
+        if not client_packet:
+            print("Did not receive an integer between 1 and 100; closing server")
+            exit(1)
 
-            client_name = client_packet["name"]
-            num2 = client_packet["data"]
-            if not (num2 >= 1 and num2 <= 100):
-                print("You must provide an integer between 1 and 100\nPlease rerun both the server and client")
-                break
+        client_name = client_packet["name"]
+        num2 = client_packet["data"]
+        if not (num2 >= 1 and num2 <= 100):
+            print("You must provide an integer between 1 and 100\nPlease rerun both the server and client")
+            exit(1)
 
-            print(f"{host_name} has connected to {client_name}\n{client_name} sent over {num2} which, when summed with {host_name}'s number of {num1}, results in {num1+num2}")
-            host_packet = {'name':host_name, 'data':num1}
-            client.sendall(json.dumps(host_packet).encode())
-            break
+        print(f"{host_name} has connected to {client_name}\n{client_name} sent over {num2} which, when summed with {host_name}'s number of {num1}, results in {num1+num2}")
+        host_packet = {'name':host_name, 'data':num1}
+        client.sendall(json.dumps(host_packet).encode())
